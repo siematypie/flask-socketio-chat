e3b9aa899ddaa41a6910e0cfa9d8582585368c3f
siematypie <mkkc12@gmail.com>
2017-04-15 18:21:31 +0200

    Add connect and disconnect events, show active and delete disconnected users sids on site

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, session, abort
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, request, url_for, redirect, flash, session, abort, jsonify
+from flask_socketio import SocketIO, send, emit, join_room
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12)
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
+ids = []
+test = [1,2,3,4,5]
+
+@socketio.on('join')
+def on_join():
+    ids.append(request.sid)
+    room = "hehe"
+    join_room(room)
+    # print(request.sid);
+    emit('userLoggedIn',ids[-1], room=room)
+
+
+@socketio.on('connect', namespace='/')
+def test_connect():
+    ids.append(request.sid)
+    print("connectiooon" + request.sid)
+    emit('connectedUserChange', {"data": ids}, broadcast=True)
+    # emit('message', "User is connected")
+
+@socketio.on('disconnect')
+def test_disconnect():
+    ids.remove(request.sid)
+    emit('connectedUserChange', {"data": ids}, broadcast=True)
+    print("DISCONNECTED " + request.sid)
 
 
 @app.route("/")
@@ -13,7 +37,7 @@ def index():
    return render_template("bla.html")
 
 
-@socketio.on('message')
+@socketio.on('message', namespace="/")
 def handle_message(msg, user="User"):
     msg = "<dt>{}</dt><dl>{}<dl>".format(user, msg)
     send(msg, broadcast=True)
