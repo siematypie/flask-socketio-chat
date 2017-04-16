a13e3b2526789cae83224384ce52f6a53b2c293d
siematypie <mkkc12@gmail.com>
2017-04-16 16:38:35 +0200

    Add viewing currently logged-in users

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, session, abort, jsonify
 from flask_socketio import SocketIO, send, emit, join_room
 import os
+import jinja2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12)
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
-ids = []
-test = [1,2,3,4,5]
+users = []
 
 @socketio.on('join')
-def on_join():
-    ids.append(request.sid)
+def on_join(name):
+    session['name'] = name
+    users.append(name)
     room = "hehe"
     join_room(room)
-    # print(request.sid);
-    emit('userLoggedIn',ids[-1], room=room)
+    print(users)
+    emit('connectedUserChange', {'data':users}, room=room)
 
 
-@socketio.on('connect', namespace='/')
-def test_connect():
-    ids.append(request.sid)
-    print("connectiooon" + request.sid)
-    emit('connectedUserChange', {"data": ids}, broadcast=True)
-    # emit('message', "User is connected")
+# @socketio.on('connect', namespace='/')
+# def test_connect():
+#     session['sid'] = request.sid
+#     ids.append(request.sid)
+#     print("connectiooon" + request.sid)
+#     emit('connectedUserChange', {"data": ids}, broadcast=True)
+#     # emit('message', "User is connected")
 
 @socketio.on('disconnect')
 def test_disconnect():
-    ids.remove(request.sid)
-    emit('connectedUserChange', {"data": ids}, broadcast=True)
-    print("DISCONNECTED " + request.sid)
+    users.remove(session['name'])
+    emit('connectedUserChange', {"data": users}, broadcast=True)
+    print("DISCONNECTED " + session['name'])
+
+@socketio.on('checkSessionName')
+def check_session_name():
+    if 'name' in session:
+        return session['name']
 
 
 @app.route("/")
@@ -39,7 +46,9 @@ def index():
 
 @socketio.on('message', namespace="/")
 def handle_message(msg, user="User"):
-    msg = "<dt>{}</dt><dl>{}<dl>".format(user, msg)
+    user = str(jinja2.escape(user))
+    msg = str(jinja2.escape(msg))
+    msg = "<dt>{}</dt><dd>{}<dd>".format(user, msg)
     send(msg, broadcast=True)
 
 
